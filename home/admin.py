5b8281075a2ff0bc056ccff2ef68edd1e5059497
mtencza <mtencza@cs.stonybrook.edu>
2019-03-17 18:42:12 -0400

    Registered models for admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django import forms
+from .models import Category, ServiceProvider, Service
 
-# Register your models here.
+class CategoryAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    form = CategoryAdminForm
+    list_display = ['name', 'slug']
+    readonly_fields = ['name', 'slug']
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ServiceProviderAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = ServiceProvider
+        fields = '__all__'
+
+
+class ServiceProviderAdmin(admin.ModelAdmin):
+    form = ServiceProviderAdminForm
+    list_display = ['name', 'slug', 'id']
+    readonly_fields = ['name', 'slug', 'id']
+
+admin.site.register(ServiceProvider, ServiceProviderAdmin)
+
+
+class ServiceAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Service
+        fields = '__all__'
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    form = ServiceAdminForm
+    list_display = ['name', 'slug']
+    readonly_fields = ['name', 'slug']
+
+admin.site.register(Service, ServiceAdmin)
+
